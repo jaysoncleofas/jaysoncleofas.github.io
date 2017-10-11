934a157cd443b95db95e22e159ebd2844eecef41
Jayson Cleofas <jaysoncleofas22@gmail.com>
2017-10-11 23:46:50 +0800

    changed profile picture default

--- database/migrations/2017_09_22_190651_add_avatar_to_users.php
+++ database/migrations/2017_09_22_190651_add_avatar_to_users.php
@@ -14,7 +14,7 @@ class AddAvatarToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('avatar')->default('default.jpg')->after('about');
+            $table->string('avatar')->default('ok.jpg')->after('about');
         });
     }
 
