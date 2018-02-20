450448d2b4ff5b569f38b08167ebb72a07972e11
Jayson Cleofas <jaysoncleofas22@gmail.com>
2018-02-20 21:10:45 +0800

    update projects

--- resources/views/index/projects.blade.php
+++ resources/views/index/projects.blade.php
@@ -1,21 +1,14 @@
 <div class="container">
    <section id="projects" class="pt-5 pb-5">
       <h2 class="h2-responsive section-heading text-center mb-2 wow fadeIn" data-wow-delay="0.2s">What i did?</h2>
-<<<<<<< HEAD
-      <p class="text-center wow fadeIn" data-wow-delay="0.2s">Projects that I created and some projects I contribute with.</p>
-=======
       <p class="text-center wow fadeIn" data-wow-delay="0.2s">Projects I've made and some projects that I'm working with.</p>
->>>>>>> 13cc8714f682675f7549a3ee7b00a6e8498a38a9
+
       @foreach ($projects as $project)
       <hr class="mb-3 mt-2">
       <div class="row wow fadeIn" data-wow-delay="0.2s">
          <div class="col-sm-7">
             <div class="view overlay hm-teal-slight z-depth-1 mb-2">
-<<<<<<< HEAD
                <img src="{{ asset('images/'.$project->image) }}" class="img-fluid" alt="">
-=======
-               <img src="{{ asset($project->image) }}" class="img-fluid" alt="">
->>>>>>> 13cc8714f682675f7549a3ee7b00a6e8498a38a9
                <a href="{{ url('portfolio/'.$project->title) }}">
                   <div class="mask waves-effect waves-light flex-center">
                      <p class=""></p>
